cd566227180b7d589ebb099daaf15347e905c055
Maurizio Aru <m.aru@almaviva.it>
2021-02-03 12:49:13 +0100

    first draft

---
+++ src/main/java/it/ginopc/kafka/stream/KafkaStreamReduceApplication.java
@@ -0,0 +1,13 @@
+package it.ginopc.kafka.stream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaStreamReduceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaStreamReduceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/ginopc/kafka/stream/KafkaStreamReduceApplicationTests.java
@@ -0,0 +1,13 @@
+package it.ginopc.kafka.stream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaStreamReduceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
